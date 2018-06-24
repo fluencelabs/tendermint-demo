05d0ff3af8ab223e5703b94ee69c238e23186353
Alexander Demidko <alexander@fluence.one>
2018-06-24 19:34:24 +0300

    Updated init files

--- tmdemoapp/local-cluster-delete.sh
+++ tmdemoapp/local-cluster-delete.sh
@@ -2,7 +2,7 @@ kill $(ps aux | grep 'tendermint node' | awk '{print $2}')
 kill $(ps aux | grep 'run 46.58' | awk '{print $2}')
 kill $(ps aux | grep 'judge' | awk '{print $2}')
 
-rm -rf ~/.tendermint1
-rm -rf ~/.tendermint2
-rm -rf ~/.tendermint3
-rm -rf ~/.tendermint4
+rm -rf ~/.tendermint/cluster4/1
+rm -rf ~/.tendermint/cluster4/2
+rm -rf ~/.tendermint/cluster4/3
+rm -rf ~/.tendermint/cluster4/4
--- tmdemoapp/local-cluster-init.sh
+++ tmdemoapp/local-cluster-init.sh
@@ -1,30 +1,30 @@
-tendermint init --home $HOME/.tendermint1
-tendermint init --home $HOME/.tendermint2
-tendermint init --home $HOME/.tendermint3
-tendermint init --home $HOME/.tendermint4
-# tendermint show_node_id --home $HOME/.tendermint1
-# tendermint show_node_id --home $HOME/.tendermint2
-# tendermint show_node_id --home $HOME/.tendermint3
-# tendermint show_node_id --home $HOME/.tendermint4
-# tendermint show_validator --home $HOME/.tendermint1
-# tendermint show_validator --home $HOME/.tendermint2
-# tendermint show_validator --home $HOME/.tendermint3
-# tendermint show_validator --home $HOME/.tendermint4
+tendermint init --home $HOME/.tendermint/cluster4/1
+tendermint init --home $HOME/.tendermint/cluster4/2
+tendermint init --home $HOME/.tendermint/cluster4/3
+tendermint init --home $HOME/.tendermint/cluster4/4
+# tendermint show_node_id --home $HOME/.tendermint/cluster4/1
+# tendermint show_node_id --home $HOME/.tendermint/cluster4/2
+# tendermint show_node_id --home $HOME/.tendermint/cluster4/3
+# tendermint show_node_id --home $HOME/.tendermint/cluster4/4
+# tendermint show_validator --home $HOME/.tendermint/cluster4/1
+# tendermint show_validator --home $HOME/.tendermint/cluster4/2
+# tendermint show_validator --home $HOME/.tendermint/cluster4/3
+# tendermint show_validator --home $HOME/.tendermint/cluster4/4
 
-TM_VALIDATOR1='{"pub_key":'$(tendermint show_validator --home $HOME/.tendermint1)',"power":10,"name":""}'
-TM_VALIDATOR2='{"pub_key":'$(tendermint show_validator --home $HOME/.tendermint2)',"power":10,"name":""}'
-TM_VALIDATOR3='{"pub_key":'$(tendermint show_validator --home $HOME/.tendermint3)',"power":10,"name":""}'
-TM_VALIDATOR4='{"pub_key":'$(tendermint show_validator --home $HOME/.tendermint4)',"power":10,"name":""}'
+TM_VALIDATOR1='{"pub_key":'$(tendermint show_validator --home $HOME/.tendermint/cluster4/1)',"power":10,"name":""}'
+TM_VALIDATOR2='{"pub_key":'$(tendermint show_validator --home $HOME/.tendermint/cluster4/2)',"power":10,"name":""}'
+TM_VALIDATOR3='{"pub_key":'$(tendermint show_validator --home $HOME/.tendermint/cluster4/3)',"power":10,"name":""}'
+TM_VALIDATOR4='{"pub_key":'$(tendermint show_validator --home $HOME/.tendermint/cluster4/4)',"power":10,"name":""}'
 TM_VALIDATORS=$TM_VALIDATOR1,$TM_VALIDATOR2,$TM_VALIDATOR3,$TM_VALIDATOR4
-sed -i -e 's#'$TM_VALIDATOR1'#'$TM_VALIDATORS'#g' $HOME/.tendermint1/config/genesis.json
-cp $HOME/.tendermint1/config/genesis.json $HOME/.tendermint2/config/genesis.json
-cp $HOME/.tendermint1/config/genesis.json $HOME/.tendermint3/config/genesis.json
-cp $HOME/.tendermint1/config/genesis.json $HOME/.tendermint4/config/genesis.json
-# cat $HOME/.tendermint4/config/genesis.json
+sed -i -e 's#'$TM_VALIDATOR1'#'$TM_VALIDATORS'#g' $HOME/.tendermint/cluster4/genesis.json
+cp $HOME/.tendermint/cluster4/genesis.json $HOME/.tendermint/cluster4/1/config/genesis.json
+cp $HOME/.tendermint/cluster4/genesis.json $HOME/.tendermint/cluster4/2/config/genesis.json
+cp $HOME/.tendermint/cluster4/genesis.json $HOME/.tendermint/cluster4/3/config/genesis.json
+cp $HOME/.tendermint/cluster4/genesis.json $HOME/.tendermint/cluster4/4/config/genesis.json
 
-sed -i -e 's#addr_book_strict = true#addr_book_strict = false#g' $HOME/.tendermint1/config/config.toml
-sed -i -e 's#addr_book_strict = true#addr_book_strict = false#g' $HOME/.tendermint2/config/config.toml
-sed -i -e 's#addr_book_strict = true#addr_book_strict = false#g' $HOME/.tendermint3/config/config.toml
-sed -i -e 's#addr_book_strict = true#addr_book_strict = false#g' $HOME/.tendermint4/config/config.toml
+sed -i -e 's#addr_book_strict = true#addr_book_strict = false#g' $HOME/.tendermint/cluster4/1/config/config.toml
+sed -i -e 's#addr_book_strict = true#addr_book_strict = false#g' $HOME/.tendermint/cluster4/2/config/config.toml
+sed -i -e 's#addr_book_strict = true#addr_book_strict = false#g' $HOME/.tendermint/cluster4/3/config/config.toml
+sed -i -e 's#addr_book_strict = true#addr_book_strict = false#g' $HOME/.tendermint/cluster4/4/config/config.toml
 
-export TM_PERSISTENT_PEERS=$(tendermint show_node_id --home $HOME/.tendermint1)"@0.0.0.0:46156,"$(tendermint show_node_id --home $HOME/.tendermint2)"@0.0.0.0:46256,"$(tendermint show_node_id --home $HOME/.tendermint3)"@0.0.0.0:46356,"$(tendermint show_node_id --home $HOME/.tendermint4)"@0.0.0.0:46456"
+export TM_PERSISTENT_PEERS=$(tendermint show_node_id --home $HOME/.tendermint/cluster4/1)"@0.0.0.0:46156,"$(tendermint show_node_id --home $HOME/.tendermint/cluster4/2)"@0.0.0.0:46256,"$(tendermint show_node_id --home $HOME/.tendermint/cluster4/3)"@0.0.0.0:46356,"$(tendermint show_node_id --home $HOME/.tendermint/cluster4/4)"@0.0.0.0:46456"
